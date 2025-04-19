f1f2fd00ffb6a7d2c25ed977f8e67c740c9fea7b
Rukham Khalid <rukhamkhalid08@outlook.com>
2025-04-19 09:10:04 +0500

    209

--- Django/Lectures/myapp/header/urls.py
+++ Django/Lectures/myapp/header/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from .import views
 urlpatterns = [
-    path("/index",views.index, name = 'home'),
+    path("/1home",views.home, name = 'home'),
     path("/about",views.about, name = 'about'),
     path("/profiles",views.profiles, name ='profiles'),
     path("/code",views.code, name = 'code'),
-    path("/courses",views.courses, name ='courses')
+    path("/courses",views.courses, name ='courses'),
+    path("/task",views.task, name ='task')
 ]
--- Django/Lectures/myapp/header/views.py
+++ Django/Lectures/myapp/header/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render, HttpResponse
+from django import forms
 
-def index(request):
-    return render(request , 'index.html')
+class Task(forms.Form):
+    task= forms.CharField(label='task', max_length=64, min_length=5) 
+
+# def index(request):
+    # return render(request , 'index.html')
 def about(request):
     return render(request , 'about.html')
 def profiles(request):
@@ -10,6 +14,44 @@ def courses(request):
     return render(request , 'courses.html')
 def code(request):
     return render(request , 'code.html')
+def task(request):
+    return render(request , 'task.html')
 
 
+# tasks=[]
+# def home(request):
+#     return render(request, '1home.html',{'task':tasks})
+# def task(request):
+    # if request.method=='POST':
+    #     task=request.POST.get('task')
+    #     tasks.append(task)
+    #     return render(request, 'task.html',{'task':tasks})
+    
+    
+    # if request.method=='POST':
+    #     f=Task(request.POST)
+    #     if f.is_valid():
+    #         t=f.cleaned_data['task']
+    #         tasks.append(t)
+    #         return render(request,'task.html',{'task':tasks, 'form':Task()})
+        
+    #     return render(request, 'task.html',{'task':tasks, 'form':Task()})
+    
+
+    
 # Create your views here.
+def home(request):
+    return render(request, '1home.html', {'task':request.session['tasks']})
+def task(request):
+    
+ if request.method=='POST':
+  f=Task(request.POST)
+  if f.is_valid():
+        t=f.cleaned_data['task']
+        tasks=[]
+        tasks=request.session['tasks']
+        tasks.append(t)
+        request.session['tasks']=tasks
+        return render(request, 'task.html', {'task':request.session['tasks'], 'form':Task()})
+ request.session['tasks']=list()
+ return render(request, 'task.html', {'task':request.session['tasks'], 'form':Task()})
